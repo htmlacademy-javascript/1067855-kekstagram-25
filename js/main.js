a043fa77f3008b7a26ddb910cdf246d78a391b6f
rmoongit <emil_tt@mail.ru>
2022-02-21 23:35:15 +0300

    Добавил комментарии к функциям по jsDoc.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
-//Функция, возвращающая случайное целое число из переданного диапазона включительно.
-
+/**
+ * Функция, возвращающая случайное целое число из переданного диапазона включительно.
+ *
+ * @param {number} min - первое число
+ * @param {number} max - второе число
+ *
+ */
 const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 getRandomInt(2, 3);
 
 
-//Проверка длинны строки.
-
-const checkStringLength = (string, maxLength) => string.length <= maxLength;
+/**
+ * Проверка длинны строки.
+ *
+ * @param {number} string - максимальная  строка
+ * @param {number} maxLength - макисмальная длина строки
+ */
+const checkStringLength = (string, maxLength = 140) => string.length <= maxLength;
 
 checkStringLength(1390, 140);
+
