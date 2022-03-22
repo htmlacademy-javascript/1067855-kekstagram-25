dc9495d42a613e268ff0f3f1e7cc2c68299b5ded
rmoongit <emil_tt@mail.ru>
2022-03-22 16:23:47 +0300

    7.11. Отрисуй меня полностью (часть 2) Исправил мелкие замечания. Добавил addEventListener. Перенёс библиотеку и удалил пустой файл.

--- js/gallery.js
+++ js/gallery.js
@@ -1,21 +1,24 @@
 import {showBigPicture} from './fullsize-picture.js';
-import {isEscapeKey} from './util.js';
-import {hideBigPicture} from './fullsize-picture.js';
 
-const handlerPicture = (photos) => {
-  const picture = document.querySelectorAll('.pictures .picture');
-  picture.forEach((elem, index) => {
-    elem.addEventListener('click', () => showBigPicture(photos[index]));
+const template = document.querySelector('#picture').content;
+const templatePicture = template.querySelector('.picture');
+const pictureList = document.querySelector('.pictures');
+
+
+const renderPhotos = (photosData) => {
+  const templateFragment = document.createDocumentFragment();
+  photosData.forEach(({url, likes, comments, description}) => {
+    const result = {url, likes, comments, description};
+    const photoElement = templatePicture.cloneNode(true);
+    photoElement.querySelector('.picture__img').src = url;
+    photoElement.querySelector('.picture__likes').textContent = likes;
+    photoElement.querySelector('.picture__comments').textContent = comments.length;
+    templateFragment.append(photoElement);
+    photoElement.addEventListener('click', () => showBigPicture(result));
   });
-  const closeButton = document.querySelector('.big-picture__cancel');
-  closeButton.addEventListener('click', () => hideBigPicture());
-  document.addEventListener('keydown', (evt) => {
-    if (isEscapeKey(evt)) {
-      hideBigPicture();
-    }
-  });
+  pictureList.append(templateFragment);
 };
 
 export {
-  handlerPicture
+  renderPhotos
 };
--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
-import {renderPhotos} from './miniature.js';
+import {renderPhotos} from './gallery.js';
 import {createPhotos, PHOTO_COUNT} from './data.js';
-import {handlerPicture} from './gallery.js';
-
 
 const photos = createPhotos(PHOTO_COUNT);
 
 renderPhotos(photos);
-handlerPicture(photos);
 
